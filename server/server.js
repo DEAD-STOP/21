766fc948f484b80ce7acb69653da42de7761bfda
DEAD-STOP <arstop98@gmail.com>
2022-09-18 15:19:16 -0500

    created schemas folder + files

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { typeDefs, resolvers } = require('./schemas');
+// need apolloserver
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,6 +18,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use(routes);
+
+// need to create new instance of appoloServer
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
