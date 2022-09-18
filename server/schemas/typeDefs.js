ff0631aed7fe8939740dd9d8e2f79c70170271ec
DEAD-STOP <arstop98@gmail.com>
2022-09-18 15:33:57 -0500

    typeDefs worked upon

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,6 +8,33 @@ type User {
     bookCount: Int
     savedBooks: [Book]
   }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    me: User
+  }
 `;
 
 module.exports = typeDefs;
